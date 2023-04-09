9bd0b2b791c114b9ababb07c6423ec3a571de934
torry2 <102455961+torry2@users.noreply.github.com>
2023-04-09 23:41:14 +0800

    I made some changes or some shit.

--- gcc2line.c
+++ gcc2line.c
@@ -2,78 +2,47 @@
 #include <stdlib.h>
 #include <string.h>
 
-void line( const char* file )
+void line( char* file )
 {
-	FILE * fp;
-	char c;
+    FILE * fp;
+    int c;
 
-	char *string;
-	long size;
-
-	fp = fopen(file, "r");
-	if ( fp == NULL) { puts("gcc2line: could not open file"); exit(1); }
-
-	fseek(fp, 0, SEEK_END);
-	size = ftell(fp);
-	rewind(fp);
-
-	string = (char*) malloc(size + 69);
-	if ( string == NULL ) { puts("gcc2line: malloc failed"); exit(1); }
-
-	string[size] = '\0';	
-
-	while ((c = fgetc(fp)) != EOF) 
-	{
-
-	char cc[2] = {c, '\0'};
-
-	char tab[] = "\\t";
-        char new[] = "\\n";
-        char dubble[] = "\"";
-        char single[] = "\\'";
-	char esc[] = "\\";
-
-
-		switch (c)
-		{
-			case '\t':
-				strcat(string, tab);
-				break;
-			case '\n':
-				strcat(string, new);
-				break;
-			case '"':
-				strcat(string, dubble);
-				break;
-			case '\'':
-				strcat(string, single);
-				break;
-			case '\\':
-				strcat(string, esc);
-				break;
-			default:
-				strcat(string, cc);
-				break;
-		}
-	}
-	fclose(fp);
-
-	const char* pstring = strdup(string);
-	free(string);
-
-	const char* filename = strdup(file);
-        char* extension = strrchr(filename, '.');
-        if ( extension != NULL ) { *extension = '\0'; }
-
-	printf("printf \'%s\' | gcc -x c -O -Wall -o %s - && ./%s ; rm %s\n", pstring, filename, filename, filename);
-
+    fp = fopen(file, "r");
+    if ( fp == NULL) { perror("gcc2line: could not open file"); exit(1); }
+ 
+    fputs("printf '", stdout);
+    while ((c = fgetc(fp)) != EOF) 
+    {
+        switch (c)
+        {
+            case '\t':
+                fputs("\\t", stdout);
+                break;
+            case '\n':
+                fputs("\\n", stdout);
+                break;
+            case '\'':
+                fputs("\\'", stdout);
+                break;
+            case '\\':
+                fputs("\\\\", stdout);
+                break;
+            default:
+                putchar(c);
+                break;
+        }
+    }
+    fclose(fp);
+    char* extension = strrchr(file, '.');
+    if ( extension != NULL ) { *extension = '\0'; }
+    fprintf(stdout, "' | gcc -x c -O -Wall -o %s - && ./%s ; rm %s\n", file, file, file);
 }
-
-void usage() { puts("gcc2line: not enough arguments"); exit(0); }
-
+ 
+void usage() { fputs("gcc2line: not enough arguments\n", stderr); exit(0); }
+ 
 int main(int argc, char* argv[])
 {
-	if ( argc != 2 ) { usage(); }
-	line(argv[1]);
-	return 0;
+    if ( argc != 2 ) { usage(); }
+    line(argv[1]);
+    return 0;
 }
